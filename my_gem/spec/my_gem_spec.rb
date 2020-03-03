65d3cdbc9a38e2adc6469685039a0e3dbfe083d3
Cristian <alu010101020730@ull.edu.es>
2020-03-03 23:05:08 +0000

    arreglando

--- my_gem/spec/my_gem_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe MyGem do
-  it "has a version number" do
-    expect(MyGem::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
